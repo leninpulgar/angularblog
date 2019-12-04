7c0a088041418d65e8310d9d84b62f3e49a8f25a
Lenin Pulgar <lenindpulgar@gmail.com>
2019-12-04 12:55:43 -0300

    second commit

---
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { PostService } from '../post.service';
+
+import {Post} from '../post'
+
+@Component({
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.css']
+})
+export class PostDetailComponent implements OnInit {
+  
+  post: Post
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) { }
+
+  ngOnInit() {
+    this.getPost()
+    console.log(this)
+  }
+
+  getPost(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.postService.getPostData(id).subscribe(post => (this.post = post))
+  }
+
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDK_rRHU24LbY7_GuEFAwAxbiX0skr70SY",
+    authDomain: "blog-trial-b4008.firebaseapp.com",
+    databaseURL: "https://blog-trial-b4008.firebaseio.com",
+    projectId: "blog-trial-b4008",
+    storageBucket: "blog-trial-b4008.appspot.com",
+    messagingSenderId: "1012443794366"
+  },
 };
 
 /*
